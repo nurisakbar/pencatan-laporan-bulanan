23a1e149af8a84130ee62160974f394a21f18f47
Nuris Akbar <nuris.akbar@gmail.com>
2022-06-11 23:21:20 +0700

    Update ReportMonthLyController.php

--- app/Http/Controllers/ReportMonthLyController.php
+++ app/Http/Controllers/ReportMonthLyController.php
@@ -10,8 +10,8 @@ class ReportMonthLyController extends Controller
 {
     public function index(){
         $data['activities'] = Activity::where('user_id',\Auth::user()->id)->get();
-        $pdf = PDF::loadView('laporan-bulanan.index', $data)->setPaper('A4', 'potrait');
-        return $pdf->stream('laporan-bulanan.pdf');
-        // return view('laporan-bulanan.index',$data);
+        // $pdf = PDF::loadView('laporan-bulanan.index', $data)->setPaper('A4', 'potrait');
+        // return $pdf->stream('laporan-bulanan.pdf');
+        return view('laporan-bulanan.index',$data);
     }
 }
